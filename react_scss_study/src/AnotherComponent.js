ea220de73362b9118dcd0777ca236f3288cdf728
daekyu.park <mamma1234@gmail.com>
2022-04-26 09:46:52 +0900

    update

--- react_scss_study/src/AnotherComponent.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './AnotherComponent.module.css';
-
-const AnotherComponent = () => {
-  return <div className={styles.wrapper}>What about CSS Module?</div>;
-};
-
-export default AnotherComponent;
